88ad9ba6d009c543ffab48c2929c37a02cd68d53
Santa claus <santa@nortpole.com>
2021-04-09 23:18:05 +0300

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,28 @@ function confirm() {
         return true
     }
 }
+
+function evaluate(){
+    year = document.getElementById("year").value;
+     CC = parseInt(year.substring(0,2));
+     YY = parseInt(year.substring(2,4)); 
+     MM = parseInt(document.getElementById("month").value);
+     DD = parseInt(document.getElementById("date").value);
+     d =(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+     console.log(d)
+     return (Math.floor(d));
+}
+
+function getGender(){
+    var genders = document.getElementsByName("gender");
+    if (genders[0].checked == true){
+        var gender = "male";
+    }
+    else if (gender[1].checked == true){
+        var gender = "female";
+    }
+    else {
+        return false
+    }
+    
+}
