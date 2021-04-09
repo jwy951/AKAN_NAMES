7a937d6d0b08c91b5f2d0f98b759524f73ee9f52
Santa claus <santa@nortpole.com>
2021-04-09 21:36:54 +0300

    add function

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var CC,YY, MM, DD, dayValue;
+var dayNames = ["SUnday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames =["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames =["Akosu", "adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+/*function confirm() {
+    var genders = document.getElementsByName("gender");
+    if (document.myForm.year.value == "" || document.myForm.year.value.length ! = 4 || document.myForm.year.value >2100 || document.myForm.year.value<=1900) {
+
+    }
+}
