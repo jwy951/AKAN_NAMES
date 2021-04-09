20b52872a27fba8805f391fe5b1c7d90a2601e65
Santa claus <santa@nortpole.com>
2021-04-09 21:48:01 +0300

    add if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-var CC,YY, MM, DD, dayValue;
+var CC, YY, MM, DD, dayValue;
 var dayNames = ["SUnday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames =["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames =["Akosu", "adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-/*function confirm() {
+function confirm() {
     var genders = document.getElementsByName("gender");
-    if (document.myForm.year.value == "" || document.myForm.year.value.length ! = 4 || document.myForm.year.value >2100 || document.myForm.year.value<=1900) {
-
+    if (document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value >2100 || document.myForm.year.value<=1900) {
+        alert("please provide a valid year of birth!");
+        document.myForm.year.focus();
+        return false
     }
 }
