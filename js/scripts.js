a1d57233048554c77134ac8eea44ce225d826ba6
Santa claus <santa@nortpole.com>
2021-04-09 22:17:33 +0300

    add else if and else

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,22 @@ function confirm() {
         document.myForm.year.focus();
         return false
     }
+    else if (DocumentFragment.myForm.moth.value== ""|| isNaN(document.myForm.month.value ) || document.myForm.month.value.length !=2 || document.myForm.month.value > 12 || document.myForm.month.value <= 0) {
+        alert("please provide your month of birth!");
+        document.myForm,year.focus();
+        return false
+    }
+    else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
+        document.myForm.month.value.length != 2|| document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
+        alert( "Please provide a valid date that you were born in!" );
+        document.myForm.day.focus() ;
+        return false;
+    }
+    else if (genders(0).cheecked==false && genders(1).checked==false){
+        alert("you must select male or female");
+        return false
+    }
+    else{
+        return true
+    }
 }
